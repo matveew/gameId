bb3edd1f5dcf9b5ac4b22f5d3838910da8b29ba6
Rick <matwei1970@gmail.com>
2020-01-07 21:48:53 +0200

    added new warrior: wizard

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,8 @@
 import fight.Arena;
 import warrior.Robber;
+import warrior.Wizard;
 
 public class Test {
-
-
 
 
     public static void main(String[] args) {
@@ -11,10 +10,11 @@ public class Test {
 
         Robber conan = new Robber("Conan", 155, 0, 10, 6);
 
+        Wizard gandalf = new Wizard("Gandalf", 80, 0, 11, 8);
 
         Arena arena = new Arena();
 
-        arena.deathBattle(robert, conan);
+        arena.deathBattle(robert, gandalf);
 
     }
 }
---
+++ src/main/java/warrior/Wizard.java
@@ -0,0 +1,26 @@
+package warrior;
+
+public class Wizard extends Warrior {
+
+
+    public Wizard(String name, int health, int armor, int damageMax, int damageMin) {
+        super(name, health, armor, damageMax, damageMin);
+    }
+
+
+    public int attack() {
+
+
+        if (Math.random() < 0.2)
+            return specialAttack();
+        else
+            return (int) (getDamageMin() + (getDamageMax() - getDamageMin()) * Math.random());
+
+
+    }
+
+    public int specialAttack() {
+        return getDamageMax() * ( getName().length() / 2);
+
+    }
+}
